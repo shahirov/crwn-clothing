a3c5ce1ebfd59ca774fc11e9ff62c235908e1ec3
shahirov <arafat.shakhirov@gmail.com>
2019-12-16 22:44:45 +0500

    Add action for tracking cart items

--- src/features/cart/cart-slice.ts
+++ src/features/cart/cart-slice.ts
@@ -1,23 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  quantity: number;
+}
 
 interface CartState {
-  hidden: boolean
+  hidden: boolean;
+  cartItems: CartItem[];
 }
 
 const initialState: CartState = {
-  hidden: true
-}
+  hidden: true,
+  cartItems: []
+};
 
 const cart = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
     toggleCartHidden(state: CartState) {
-      state.hidden = !state.hidden
+      state.hidden = !state.hidden;
+    },
+    addItem(state: CartState, action: PayloadAction<CartItem>) {
+      const idx = state.cartItems.findIndex(
+        item => item.id === action.payload.id
+      );
+
+      if (idx) {
+        state.cartItems[idx].quantity += action.payload.quantity;
+        state.cartItems[idx].price += action.payload.price;
+      } else {
+        state.cartItems.push(action.payload);
+      }
     }
   }
-})
+});
 
-export const { toggleCartHidden } = cart.actions
+export const { toggleCartHidden, addItem } = cart.actions;
 
-export default cart.reducer
+export default cart.reducer;
