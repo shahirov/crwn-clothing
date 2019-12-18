8f9682b618fbd808286edc78cbc002015481c86f
shahirov <arafat.shakhirov@gmail.com>
2019-12-18 20:44:43 +0500

    Add redux-persist library into app

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './index.css'
 import App from './app/App'
-import store from './app/store'
+import store, { persistor } from './app/store'
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById('root')
